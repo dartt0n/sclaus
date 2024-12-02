b7ec88861d11e6c11e454d1bbcae912a246a644a
Anton Kudryavtsev <dartt0n@ya.ru>
2024-12-03 02:32:52 +0300

    build: add telegramium dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,10 @@ object Dependencies {
 
   lazy val munit = "org.scalameta" %% "munit" % "1.0.0" % Test
 
-  lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.7"
+  lazy val catsEffect      = "org.typelevel"         %% "cats-effect"      % "3.5.7"
+  lazy val telegramiumCore = "io.github.apimorphism" %% "telegramium-core" % "9.800.0"
+  lazy val telegramiumHigh = "io.github.apimorphism" %% "telegramium-high" % "9.800.0"
 
-  lazy val all = Seq(catsEffect, munit)
+  lazy val all = Seq(catsEffect, munit, telegramiumCore, telegramiumHigh)
 
 }
